a8d9c07b3bc94490a35b61b9968f6795f9adae66
Naveen M K <naveen521kk@gmail.com>
2026-07-22 21:23:15 +0530

    fix: url typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tools.naveemk.me",
+  site: "https://tools.naveenmk.me",
   vite: {
     define: {
       __APP_VERSION__: JSON.stringify(pkg.version),
